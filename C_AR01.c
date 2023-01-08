98b2b464f973d29e2a3527537a7e75a76608b67f
WDahua <93331920+408410075@users.noreply.github.com>
2023-01-08 23:32:54 +0800

    Add files via upload

---
+++ C_AR01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+// 一維陣列反轉
+int main(){
+    char array[100];    // input array
+    int length; // array length
+    while(gets(array) != NULL){
+        length = strlen(array);
+        for(int i = length-1; i >= 0; i--){
+            if(array[i] == ' '){
+                for(int j = i+1; array[j] != ' ' && array[j] != 0;j++){
+                    printf("%c", array[j]);;
+                }
+                printf(" ");
+            }
+        }
+        for(int i = 0; array[i] != ' '; i++){
+            printf("%c", array[i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
